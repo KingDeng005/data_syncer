99a164307d2c06384fd6f1021fc39719dfeff198
kingdeng005 <fuheng.deng@tusimple.ai>
2018-05-18 14:23:43 -0700

    update upload file

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -2,4 +2,4 @@
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 TOP_DIR="$( cd "${SCRIPT_DIR}"/.. && pwd)"
 umask 002
-rsync -r  --exclude='.git' ${TOP_DIR}/ /mnt/truenas/scratch/ds_src/data-syncer --update --delete
+rsync -r  --exclude='.git' --exclude='.swp' --exclude='.pyc' ${TOP_DIR}/ /mnt/truenas/scratch/ds_src/data-syncer --update --delete
